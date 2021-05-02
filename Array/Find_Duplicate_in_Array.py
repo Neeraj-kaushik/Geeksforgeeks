74ebd0eae137cb8b6d793a59d1e13cc3cae0b500
Neeraj <nkthecoder@gmail.com>
2021-05-02 10:33:38 +0530

    array

--- Array/Find_Duplicate_in_Array.py
+++ Array/Find_Duplicate_in_Array.py
@@ -3,12 +3,7 @@ a=[]
 li=[int(x) for x in input().split()]
 for i in range(len(li)-1):
     for j in range(i+1,len(li)):
-        if i==j:
-            a.append(li[i])
-x=len(a)
-for i in a:
-    if x==0:
-        print('-1')
-    else:
-        print(i,end=' ')
+        if li[i]==li[j]:
+            print(li[i],end=' ')
+
 
