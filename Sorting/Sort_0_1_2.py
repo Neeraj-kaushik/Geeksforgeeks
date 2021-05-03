a9be810aada44a01f38a453972129afb51d7ed0e
Neeraj <nkthecoder@gmail.com>
2021-05-03 19:27:03 +0530

    sorting

--- Sorting/Sort_0_1_2.py
+++ Sorting/Sort_0_1_2.py
@@ -1,12 +1,12 @@
-def bubble_sort(n,li):
+def sort012(n,li):
     l=len(li)
     for i in range(l-1):
         for j in range(i+1,l):
-            if li[i]>li[i+1]:
-                li[i],li[i+1]=li[i+1],li[i]
+            if li[i]>li[j]:
+                li[i],li[j]=li[j],li[i]
     print(li)
-
+    
 
 n=int(input())
 li=[int(x) for x in input().split()]
-bubble_sort(n,li)
+sort012(n,li)
