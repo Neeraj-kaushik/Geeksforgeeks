8073b8573a7405fcefc76ea18d4e5cb898936b34
Neeraj <nkthecoder@gmail.com>
2021-05-13 09:18:24 +0530

    String

--- String/Run_Length_Encoding.py
+++ String/Run_Length_Encoding.py
@@ -1,12 +1,14 @@
 def encoding(st):
     for i in range(len(st)-1):
-        a=0
-        for j in range(i+1,len(st)):
-            while st[i]!=st[j]:
+        j=i
+        while st[i]!=st[j] and j<len(st):
+            if st[i]!=st[j]:
                 a=j-i
-                print(st[i]+str(a),end='')
-                i=j
-            
+            else:
+                j=j+1
+        print(st[i]+str(a),end='')
+        i=j            
+        
                             
 
 
