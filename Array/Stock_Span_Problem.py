33cfd892c03f7930b04a158206be7a37cbeb916a
Neeraj <nkthecoder@gmail.com>
2021-05-19 13:56:51 +0530

    Array

---
+++ Array/Stock_Span_Problem.py
@@ -0,0 +1,18 @@
+def stock_span(li):
+    li1=[]
+    for i in range(len(li)):
+        count=1
+        for j in range(0,i):
+            if li[i]>li[j]:
+                count=count+1
+        li1.append(count)
+    for i in range(len(li1)):
+        print(li1[i],end=' ')
+
+        
+
+
+
+n=int(input())
+li=[int(x) for x in input().split()]
+stock_span(li)
