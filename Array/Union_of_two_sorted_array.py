33261fcc3de14b03a1127738aa00a4ce1bd44273
Neeraj <nkthecoder@gmail.com>
2021-05-31 10:18:12 +0530

    Array

--- Array/Union_of_two_sorted_array.py
+++ Array/Union_of_two_sorted_array.py
@@ -1,30 +1,17 @@
-def union(n,li,m,li2):
-    li3=[]
-    i=0
-    j=0
-    while i<n and j <m:
-        if li[i]<li2[j]:
+def union(n, li, m, li2):
+    li3 = []
+    for i in range(len(li)):
+        if li[i] not in li3:
             li3.append(li[i])
-            i=i+1
-        elif li2[j]<li[i]:
-            li3.append(li2[j])
-            j=j+1
-        else:
-            li3.append(li2[j])
-            i=i+1
-            j=j+1
-    while i <n:
-        li3.append(li[i])
-        i=i+1
-    while j<m:
-        li3.append(li2[j])
-        j=j+1
+    for i in range(len(li2)):
+        if li2[i] not in li3:
+            li3.append(li2[i])
+    li3 = sorted(li3)
     print(li3)
 
 
-
-n=int(input())
-li=[int(x) for x in input().split()]
-m=int(input())
-li2=[int(y) for y in input().split()]
-union(n,li,m,li2)
+n = int(input())
+li = [int(x) for x in input().split()]
+m = int(input())
+li2 = [int(y) for y in input().split()]
+union(n, li, m, li2)
