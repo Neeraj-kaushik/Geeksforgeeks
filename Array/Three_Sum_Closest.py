4fc0a01d0bb77fae24f9f3db86334003c35a72ca
Neeraj <nkthecoder@gmail.com>
2021-06-04 19:15:52 +0530

    array

--- Array/Three_Sum_Closest.py
+++ Array/Three_Sum_Closest.py
@@ -1,23 +1,19 @@
 def three_sum_closest(t, li):
     min = 1000
     for i in range(len(li)-2):
-        sum = li[i]
         for j in range(i+1, len(li)-1):
-            sum = sum+li[j]
-            for k in range(j+1, len(li)):
-                sum = sum+li[k]
+            for k in range(len(li)):
+                sum = li[i]+li[j]+li[k]
                 if sum == t:
-                    min = sum
+                    min = t
                     break
-                elif sum < t:
-                    a = -(sum-t)
+                else:
+                    sum = abs(sum)
+                    a = abs(sum-t)
                     if a < min:
                         min = a
-                elif sum > t:
-                    b = sum-t
-                    if b < min:
-                        min = b
-    print(min)
+                        b = sum
+    print(b)
 
 
 n = int(input())
