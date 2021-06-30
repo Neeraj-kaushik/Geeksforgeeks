82154e81f4a94875fb69c265c6890bb5a6cf14b2
Neeraj <nkthecoder@gmail.com>
2021-06-30 17:32:53 +0530

    array

--- Array/counting_element_in_two_array.py
+++ Array/counting_element_in_two_array.py
@@ -1,1 +1,16 @@
+def counting_element(li, li1):
+    li3 = []
+    for i in range(len(li)):
+        count = 0
+        for j in range(len(li1)):
+            if li[i] >= li1[j]:
+                count = count+1
+        li3.append(count)
+    print(li3)
+
+
 n = int(input())
+m = int(input())
+li = [int(x) for x in input().split()]
+li1 = [int(x) for x in input().split()]
+counting_element(li, li1)
